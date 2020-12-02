7813d2abcdf60648a67d3ca91355f389fae40877
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2020-12-02 08:50:34 +0100

    Adventofcode 2020 day 2

---
+++ src/main/kotlin/nl/vloris/adventofcode/advent2020/Day2.kt
@@ -0,0 +1,70 @@
+package nl.vloris.adventofcode.advent2020
+
+import nl.vloris.adventofcode.common.BaseSolver
+
+class Day2 : BaseSolver(2020, 2) {
+    override fun part1() {
+        var validPasswords = 0
+
+        for (inputLine in getInputLines()) {
+            val (policy, password) = inputLine.split(": ")
+
+            if (Policy1(policy).validate(password)) {
+                validPasswords++
+            }
+        }
+
+        println("$validPasswords passwords are valid")
+    }
+
+    override fun part2() {
+        var validPasswords = 0
+
+        for (inputLine in getInputLines()) {
+            val (policy, password) = inputLine.split(": ")
+
+            if (Policy2(policy).validate(password)) {
+                validPasswords++
+            }
+        }
+
+        println("$validPasswords passwords are valid")
+    }
+}
+
+data class Policy1(val policy: String) {
+    private val min: Int
+    private val max: Int
+    private val letter: Char
+
+    init {
+        val split = policy.split('-', ' ')
+        min = split[0].toInt()
+        max = split[1].toInt()
+        letter = split[2][0]
+    }
+
+    fun validate(password: String): Boolean {
+        val count = password.filter { it == letter }.count()
+        return count in min..max
+    }
+}
+
+data class Policy2(val policy: String) {
+    private val position1: Int
+    private val position2: Int
+    private val letter: Char
+
+    init {
+        val split = policy.split('-', ' ')
+        position1 = split[0].toInt() - 1
+        position2 = split[1].toInt() - 1
+        letter = split[2][0]
+    }
+
+    fun validate(password: String): Boolean {
+        return (password[position1] == letter)
+            .xor(password[position2] == letter)
+    }
+}
+
--- src/main/kotlin/nl/vloris/adventofcode/main.kt
+++ src/main/kotlin/nl/vloris/adventofcode/main.kt
@@ -3,5 +3,5 @@ package nl.vloris.adventofcode
 import nl.vloris.adventofcode.advent2020.*
 
 fun main() {
-    Day1().run()
+    Day2().run()
 }

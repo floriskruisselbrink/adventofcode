85eaaa334530fe01421b0b6084d1bbd386492eda
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2020-12-24 09:49:20 +0100

    Adventofcode 2020 day 24 (part 2)

--- src/main/kotlin/nl/vloris/adventofcode/advent2020/Day24.kt
+++ src/main/kotlin/nl/vloris/adventofcode/advent2020/Day24.kt
@@ -48,7 +48,13 @@ object Day24 : BaseSolver(2020, 24) {
     override fun part1(): Int =
         getInputLines().map(::parseInstruction).let(::parseInstructions).size
 
-    override fun part2(): Int = 0
+    override fun part2(): Int {
+        var grid = getInputLines().map(::parseInstruction).let(::parseInstructions)
+
+        repeat(100) { grid = nextIteration(grid) }
+
+        return grid.size
+    }
 
     private fun parseInstruction(instruction: String) = sequence {
         val regex = """[ns]?[ew]""".toRegex()
@@ -60,14 +66,45 @@ object Day24 : BaseSolver(2020, 24) {
     }
 
     private fun parseInstructions(instructions: List<Sequence<DirectionHex>>): Set<PointHex> {
-        val map = mutableSetOf<PointHex>()
+        val grid = mutableSetOf<PointHex>()
         instructions.forEach { instructionSet ->
             val point = instructionSet.fold(PointHex(0, 0, 0)) { p, d -> p + d }
-            if (!map.remove(point)) {
-                map.add(point)
+            if (!grid.remove(point)) {
+                grid.add(point)
             }
         }
 
-        return map
+        return grid
+    }
+
+    private fun nextIteration(grid: Set<PointHex>): Set<PointHex> {
+        val nextGrid = grid.toMutableSet()
+
+        // find all black tiles with zero or more than 2 black neighbours
+        grid.filter { tile ->
+            val blackNeighbours = tile.neighbours().filter { grid.contains(it) }.size
+            blackNeighbours == 0 || blackNeighbours > 2
+        }.let {
+            nextGrid.removeAll(it)
+        }
+
+        grid
+            // find all white tiles adjacent to one or more black tiles
+            .asSequence()
+            .map { it.neighbours() }
+            .flatten().toSet()
+            .filterNot { grid.contains(it) }
+            // filter only white tiles with exactly 2 black neighbours
+            .filter { tile ->
+                val blackNeighbours = tile.neighbours().filter { grid.contains(it) }.size
+                blackNeighbours == 2
+            }
+            .toList()
+            // make them black
+            .let {
+                nextGrid.addAll(it)
+            }
+
+        return nextGrid
     }
 }
